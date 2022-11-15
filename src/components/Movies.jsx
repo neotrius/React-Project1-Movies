d81c9dbe41331e4e6bbe8dec8d8a0b996c12c23c
Yeugen1 <yeugen@gmail.com>
2022-11-15 14:12:09 +0100

    updated code for failure queries

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,12 +1,14 @@
 import Movie from './Movie'
 
 function Movies(props) {
-    const {movies} = props
+    const {movies = []} = props
 
     return <div className="movies">
-        {movies.map(movie => (
+        {movies.length ? movies.map(movie => (
             <Movie key={movie.imdbID} {...movie}/>
-        ))}</div>
+        )) : (
+            <h3>Nothing found</h3>
+        )}</div>
 }
 
 export default Movies
